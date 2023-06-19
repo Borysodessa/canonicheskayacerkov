593e14e83b424ccb511db92924f4dd260cd7d0d5
BorysGull <chayka2021@yahoo.com>
2023-06-20 01:37:53 +0300

    сделан overlay

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 const link = document.querySelector(".schedule-btn");
 const popap = document.querySelector(".modal-schedule");
 const closePopap = document.querySelector(".modal-close");
+const overlay = document.querySelector(".overlay");
 
 link.addEventListener("click", function (evt) {
-  evt.preventDefault(evt);
+  evt.preventDefault();
   popap.classList.remove("visually-hidden");
+  overlay.classList.remove("visually-hidden");
 });
 
 closePopap.addEventListener("click", function (evt) {
   evt.preventDefault();
   popap.classList.add("visually-hidden");
+  overlay.classList.add("visually-hidden");
+});
+
+overlay.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popap.classList.add("visually-hidden");
+  overlay.classList.add("visually-hidden");
 });
