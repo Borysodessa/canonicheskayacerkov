7f35c59cfc77261b40ff22e9ed6d73b4f221d1e5
BorysGull <chayka2021@yahoo.com>
2023-06-22 13:53:34 +0300

    сделан Украина тулбар всплывающяя страница

--- index.js
+++ index.js
@@ -2,6 +2,21 @@ const link = document.querySelector(".schedule-btn");
 const popap = document.querySelector(".modal-schedule");
 const closePopap = document.querySelector(".modal-close");
 const overlay = document.querySelector(".overlay");
+const ukraine = document.querySelector(".ukraine");
+const eparchysUkrainWrap = document.querySelector(".eparchys-Ukrain__wrap");
+const eparchysUkrainClose = document.querySelector(".eparchys-Ukrain-close");
+
+ukraine.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  eparchysUkrainWrap.classList.remove("visually-hidden");
+  overlay.classList.remove("visually-hidden");
+});
+
+eparchysUkrainClose.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  eparchysUkrainWrap.classList.add("visually-hidden");
+  overlay.classList.add("visually-hidden");
+});
 
 link.addEventListener("click", function (evt) {
   evt.preventDefault();
@@ -17,6 +32,7 @@ closePopap.addEventListener("click", function (evt) {
 
 overlay.addEventListener("click", function (evt) {
   evt.preventDefault();
+  eparchysUkrainWrap.classList.add("visually-hidden");
   popap.classList.add("visually-hidden");
   overlay.classList.add("visually-hidden");
 });
